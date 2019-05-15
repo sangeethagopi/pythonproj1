51a50ac0e5a74de3d76e44a54903e64df0adf547
sangeethagopi <50619394+sangeethagopi@users.noreply.github.com>
2019-05-15 11:15:47 +0530

    Create exp20.py

---
+++ exp20.py
@@ -0,0 +1,7 @@
+ch = input()
+if(ch == 'a' or ch == 'e' or ch == 'i' or ch == 'o' or ch == 'u'):
+    print("Vowel")
+elif(ch != 'a' or ch != 'e' or ch != 'i' or ch != 'o' or ch != 'u'):
+    print("Consonant")
+else:
+    print("invalid")
